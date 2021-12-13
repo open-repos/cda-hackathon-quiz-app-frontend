66a4c6ae7e8a73cf9ac7baf4dddaaa081a20c7b9
acapai <andria.c@eronservice.fr>
2021-12-13 18:01:38 +0100

    *Titre commit: Ajout des fonts et mise en place de l'image issue des maquettes *Changement(s) effectué(s): - creation d'un dossier font - import des fontawesome - modification de la HomePage

--- Hackathon-Quiz-App/src/js/App.jsx
+++ Hackathon-Quiz-App/src/js/App.jsx
@@ -1,45 +1,29 @@
-import { useState } from 'react'
-import logo from '../assets/logo.svg'
-import '../css/App.css'
+import { useState } from "react";
+import logo from "../assets/img-main.png";
+import "../css/App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
+    <div>
+      <header>
+        <p>Home</p>
+        <p>Games</p>
+        <p>Historique</p>
       </header>
+      <div className="general">
+        <main>
+          <h1 id="main-title">Quiz Game</h1>
+          <img src={logo} alt="image-bonhomme" class="img-accueil" />
+          <h2>Règles du jeux</h2>
+          <ul>
+            <li>Règle 1:</li>
+            <li>Règle 2:</li>
+            <li>Règle 3:</li>
+          </ul>
+        </main>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
