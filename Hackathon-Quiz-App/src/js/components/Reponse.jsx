6624e4935e25730b111bf0fa34e0fc7beae0a9be
Gass <madebygass@gmail.com>
2021-12-15 16:55:38 +0100

    [feat] Create component reponse

---
+++ Hackathon-Quiz-App/src/js/components/Reponse.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../css/Reponse.css"
+
+function Reponse() {
+  return (
+    <div className="reponse">
+      <p id="main-reponse">Search engine optimization  </p> 
+    </div>
+  );
+}
+
+export default Reponse;
