afd4a970377e77e2b98844746447404c48c52aeb
ailine2020 <ailine.soto@outlook.fr>
2021-12-16 00:55:40 +0100

    [WIP] Page game: categorie quesion

--- Hackathon-Quiz-App/src/js/views/GamesPage.jsx
+++ Hackathon-Quiz-App/src/js/views/GamesPage.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { useGetQuestionsQuery } from "../services/gameApi"
+import React from "react";
+import { useGetQuestionsQuery } from "../services/gameApi";
 
+import "../../css/GamesPage.css";
+
+import Title from "../components/Title";
+import BackgroundThree from "../components/BackgroundThree";
+import CategoryQuestion from "../components/CategoryQuestion";
 
 function GamesPage() {
-    const data = useGetQuestionsQuery('DevOps')
-    console.log(data);
-    
-    return (
-        <div>
-            
+  const data = useGetQuestionsQuery("DevOps");
+  console.log(data);
+
+  return (
+    <BackgroundThree>
+      <div className="grid-container">
+        <div className="grid-child-left">
+          <Title />
+          <h2>SELECTIONNE LE THEME DE TES QUESTIONS</h2>
+          {/* <CategoryQuestion /> */}
+          <div className="lines-left">
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+          </div>
         </div>
-    )
+
+        <div className="grid-child-right">
+          <div className="lines-right">
+            <div className="line-1"></div>
+            <div className="line-2"></div>
+          </div>
+        </div>
+      </div>
+    </BackgroundThree>
+  );
 }
 
-export default GamesPage
+export default GamesPage;
