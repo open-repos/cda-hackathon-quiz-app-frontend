de7eaf0f4174267d43944454c3239ea3e8739b52
ailine2020 <ailine.soto@outlook.fr>
2021-12-15 21:14:37 +0100

    [WIP] Component : category-question

---
+++ Hackathon-Quiz-App/src/js/components/CategoryQuestion.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../css/CategoryQuestion.css"
+
+function CategoryQuestion() {
+  return (
+  <div className ="category">
+    <img src='/src/assets/devops-people.png' alt="devops-people" className="people"/>
+    <img src='/src/assets/front-people.png' alt="front-people" className="people"/>
+    <img src='/src/assets/back-people.png' alt="back-people" className="people"/>
+  </div>
+  );
+}
+
+export default CategoryQuestion;
