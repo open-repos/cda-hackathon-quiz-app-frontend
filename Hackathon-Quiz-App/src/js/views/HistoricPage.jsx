efb356d27ad139a530cec423720b6d402c6e8042
Gass <madebygass@gmail.com>
2021-12-16 23:02:11 +0100

    [feat] WIP component myhistory

--- Hackathon-Quiz-App/src/js/views/HistoricPage.jsx
+++ Hackathon-Quiz-App/src/js/views/HistoricPage.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { useGetHistoryQuery } from "../services/gameApi"
+import Myhistoric from "../components/Myhistory"
+import Title from "../components/Title"
+import "../../css/HistoricPage.css"
 
 function HistoricPage() {
     return (
         <div>
-            
+            <Title />
+            <Myhistoric />
         </div>
     )
 }
