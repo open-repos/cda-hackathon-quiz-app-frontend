214336fd2d473977be439079f2299bb6132587ce
ailine2020 <ailine.soto@outlook.fr>
2021-12-17 11:22:47 +0100

    Component Choose-response

---
+++ Hackathon-Quiz-App/src/js/views/ChooseResponse.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import Title from "../components/Title";
+import BackgroundThree from "../components/BackgroundThree";
+import ChooseResponse from "../components/ChooseResponse";
+
+function ChooseResponse() {
+  return (
+    <BackgroundThree>
+      <div className="container-el">
+        <div className="title-lines">
+          <div>
+            <Title />
+          </div>
+          <div className="lines-right gamesquestions">
+            <div className="line-1 gamesquestions"></div>
+            <div className="line-2 gamesquestions"></div>
+          </div>
+        </div>
+        <div className="categories">
+          <ChooseResponse />
+        </div>
+        <div className="lines-left gamesquestions">
+          <div className="line-1 gamesquestions"></div>
+          <div className="line-2 gamesquestions"></div>
+        </div>
+      </div>
+    </BackgroundThree>
+  );
+}
+
+export default ChooseResponse;
