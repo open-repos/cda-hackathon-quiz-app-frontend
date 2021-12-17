8b9f6c4a736b4aa9fae686d594ad0a06c9f9e16d
acapai <andria.c@eronservice.fr>
2021-12-17 08:06:50 +0100

    *Titre commit: Amelioration architecture et css

--- Hackathon-Quiz-App/src/js/components/BackgroundThree.jsx
+++ Hackathon-Quiz-App/src/js/components/BackgroundThree.jsx
@@ -4,7 +4,8 @@ function BackGroundThree({children}) {
     const bgPage ={
         minHeight:"100%",
         minWidth:"100%",
-        background: "linear-gradient(#F52D7E 0%, #292D3E 4.91%, #292D3E 95.02%, #F52D7E 100%)"
+        background: "linear-gradient(#F52D7E 0%, #292D3E 4.91%, #292D3E 95.02%, #F52D7E 100%)",
+        // overflow: "hidden",
       }
 
     return (
--- Hackathon-Quiz-App/src/js/views/ChooseCategoryQuestion.jsx
+++ Hackathon-Quiz-App/src/js/views/ChooseCategoryQuestion.jsx
@@ -1,19 +1,33 @@
 import React from "react";
 
-import "../../css/GamesPage.css";
+import "../../css/GamesPageBgThree.css";
 
 import Title from "../components/Title";
 import BackgroundThree from "../components/BackgroundThree";
 import CategoryQuestion from "../components/CategoryQuestion";
 
-
 function ChooseCategoryQuestion() {
-
-
-
   return (
     <BackgroundThree>
-      <div className="grid-container">
+      <div className="container-el">
+        <div className="title-lines">
+          <div>
+            <Title />
+          </div>
+          <div className="lines-right gamesquestions">
+            <div className="line-1 gamesquestions"></div>
+            <div className="line-2 gamesquestions"></div>
+          </div>
+        </div>
+        <div className="categories">
+          <CategoryQuestion />
+        </div>
+        <div className="lines-left gamesquestions">
+          <div className="line-1 gamesquestions"></div>
+          <div className="line-2 gamesquestions"></div>
+        </div>
+      </div>
+      {/* <div className="grid-container">
         <div className="grid-child-left">
           <Title />
           <CategoryQuestion />
@@ -29,7 +43,7 @@ function ChooseCategoryQuestion() {
             <div className="line-2"></div>
           </div>
         </div>
-      </div>
+      // </div> */}
     </BackgroundThree>
   );
 }
