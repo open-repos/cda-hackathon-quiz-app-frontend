98610dc1966d2e1d7b4b6a8906aa894d14eb1aa9
Nassim Fernane <nasfernane@gmail.com>
2021-12-17 11:54:01 +0100

    fix background

--- Hackathon-Quiz-App/src/js/components/BackgroundThree.jsx
+++ Hackathon-Quiz-App/src/js/components/BackgroundThree.jsx
@@ -5,7 +5,7 @@ function BackGroundThree({children}) {
         minHeight:"100%",
         minWidth:"100%",
         background: "linear-gradient(#F52D7E 0%, #292D3E 4.91%, #292D3E 95.02%, #F52D7E 100%)",
-        // overflow: "hidden",
+        overflow: "hidden",
       }
 
     return (
