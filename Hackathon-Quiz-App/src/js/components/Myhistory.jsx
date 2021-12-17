bfa5c759c3f41b8214380ec35998d9f26fbceb7d
Gass <madebygass@gmail.com>
2021-12-17 11:34:33 +0100

    [feat] WIP component Myhistory

--- Hackathon-Quiz-App/src/js/components/Myhistory.jsx
+++ Hackathon-Quiz-App/src/js/components/Myhistory.jsx
@@ -4,28 +4,36 @@ import "../../css/Myhistory.css"
 function Myhistory() {
 
   const [parties, setParties] = useState(null);
-  const player = 'andria'
+
   // we will use async/awiat to fetch this data
   async function getData() {
+    const player = 'andria'
     const response = await fetch(`https://api.quizz-game.andriacapai.com/v1/history/${player}`);
     const data = await response.json();
-
+    console.log(reponse);
     // store the data into our parties variables
     setParties(data);
-    console.log(parties)
   }
 
   //add the use
   useEffect(() => {
     getData();
-    
   }, []); // place the setBooks function in this array
 
   return (
-    <div>
-      <span className="mainMyhistory"> '{parties}' </span>
+    <div className="mainMyhistory">
+      {parties && (
+        <div className="parties">
+          {parties.map((partie, index) => (
+            <div key={index}>
+              <h2>{partie.name}</h2>
+            </div>
+          ))}
+
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
 export default Myhistory;
